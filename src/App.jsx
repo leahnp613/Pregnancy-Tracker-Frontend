3f3c035c5bed0c16903e4b2de405a28f26101000
leahnp613 <leahnagarpowers@gmail.com>
2024-01-01 12:18:02 -0500

    editing App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,24 +8,20 @@ function App() {
   return (
     <div className="App">
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a href="https://www.svgrepo.com/svg/35764/pregnant-woman-and-fetus" target="_blank">
+          <img src="https://www.svgrepo.com/svg/35764/pregnant-woman-and-fetus" className="logo" alt="Pregnant Woman and Fetus SVG" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Pregnancy Tracker</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button>Create A Free Profile Today!
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Created by Leah Nagarpowers
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Track your pregnancy each week with this tool!
       </p>
     </div>
   )
